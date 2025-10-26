69ea1d07854026d8b63ec94b78582c5fd11ee0be
Philipe Schiavon <philipe.schiavon@gmail.com>
2025-10-26 16:24:17 +0000

    embed este vídeo: https://youtu.be/Dj5SwhQpyC8 (_for element img_)

--- src/app/_components/community-video.tsx
+++ src/app/_components/community-video.tsx
@@ -4,7 +4,7 @@ export function CommunityVideo() {
             <div className="aspect-w-16 aspect-h-9">
             <iframe
                 className="w-full h-full"
-                src="https://www.youtube.com/embed/gSH4MUk1P84"
+                src="https://www.youtube.com/embed/Dj5SwhQpyC8"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
