0f5488617cd5d0a1768333bf4b73369d794581f6
Philipe Schiavon <philipe.schiavon@gmail.com>
2025-10-26 16:38:13 +0000

    aumente levemente a velocidade dessa esteira animada (_for element <Feat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee-slow': 'marquee 60s linear infinite',
+        'marquee-slow': 'marquee 40s linear infinite',
       },
     },
   },
