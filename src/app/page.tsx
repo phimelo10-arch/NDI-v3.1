f33cc09234e2f1103e6c9942230483a356ee362c
Philipe Schiavon <philipe.schiavon@gmail.com>
2026-01-11 06:45:34 +0000

    mova a sessão "tipo de coisas que falamos no grupo e no DISCORD" para ab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
           <Pricing />
         </div>
         <NewMembers />
-        <TestimonialsVideo />
         <ContentShowcase />
+        <TestimonialsVideo />
         <BrotherOfTheWeek />
       </main>
       <PixelEvents />
